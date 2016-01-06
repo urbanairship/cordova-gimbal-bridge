bab797ae8a46057ced0d0c9bcd2ee81dff68f084
David Crow <jdavidcrow@gmail.com>
2016-01-06 15:26:04 -0800

    updated symlink fix to before_compile

---
+++ scripts/fix-gimbal-symlink.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -ex
+
+ln -sf platforms/ios/*/Plugins/urbanairship-gimbal-cordova/Gimbal.framework/Versions/*/Gimbal Gimbal
+ln -sf platforms/ios/*/Plugins/urbanairship-gimbal-cordova/Gimbal.framework/Versions/*/Headers Headers
