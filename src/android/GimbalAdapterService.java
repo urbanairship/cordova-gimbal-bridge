a2345d5f9b838243e837e347c0ad644afd865e99
AJ Savino <aj.savino@moxieusa.com>
2016-09-29 12:26:17 -0400

    removed threading from service and stored gimbalkey in shared preferences to increase reliability

--- src/android/GimbalAdapterService.java
+++ src/android/GimbalAdapterService.java
@@ -6,51 +6,17 @@ package com.urbanairship.cordova.gimbal;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.IBinder;
-import android.os.Looper;
-import android.os.Message;
-import android.os.Process;
+import android.preference.PreferenceManager;
 
 import com.gimbal.android.Gimbal;
 
 public class GimbalAdapterService extends Service {
 	public static final String SERVICE_PARAM_GIMBAL_KEY = "gimbalKey";
 	
-	private static final int MSG_INTENT_RECEIVED = 1;
-    private static final int MSG_INTENT_JOB_FINISHED = 2;
-	
-	private Looper serviceLooper;
-	private ServiceHandler serviceHandler;
-	
-	private final class ServiceHandler extends Handler {
-		public ServiceHandler(Looper looper){
-			super(looper);
-		}
-		@Override
-		public void handleMessage(Message msg){
-			int startId = msg.arg1;
-			switch (msg.what){
-				case MSG_INTENT_RECEIVED:
-					GimbalAdapter.shared().start();
-					break;
-				case MSG_INTENT_JOB_FINISHED:
-					GimbalAdapter.shared().stop();
-					break;
-			}
-		}
-	}
-	
 	@Override
 	public void onCreate(){
 		super.onCreate();
-		
-		HandlerThread thread = new HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND);
-		thread.start();
-		
-		serviceLooper = thread.getLooper();
-		serviceHandler = new ServiceHandler(serviceLooper);
 	}
 	
 	@Override
@@ -58,38 +24,33 @@ public class GimbalAdapterService extends Service {
 		super.onStartCommand(intent, flags, startId);
 		
 		String action = null;
+		String gimbalKey = PreferenceManager.getDefaultSharedPreferences(this).getString(SERVICE_PARAM_GIMBAL_KEY, null);
 		if (intent != null){
 			action = intent.getAction();
 			
 			//setApiKey
-			String gimbalKey = null;
 			Bundle extras = intent.getExtras();
 			if (extras != null){
 				gimbalKey = (String) extras.get(SERVICE_PARAM_GIMBAL_KEY);
+				PreferenceManager.getDefaultSharedPreferences(this).edit().putString(SERVICE_PARAM_GIMBAL_KEY, gimbalKey).commit(); 
 			}
-			if (gimbalKey == null){
-				throw new IllegalArgumentException("Missing Gimbal API key");
-			}
-			Gimbal.setApiKey(this.getApplication(), gimbalKey);
 		}
+		if (gimbalKey == null){
+			throw new IllegalArgumentException("Missing Gimbal API key");
+		}
+		Gimbal.setApiKey(this.getApplication(), gimbalKey);
 		
 		//startMonitoring
 		if (!Intent.ACTION_RUN.equals(action)){
-			Message msg = serviceHandler.obtainMessage(MSG_INTENT_RECEIVED);
-			msg.arg1 = startId;
-			msg.obj = intent;
-			serviceHandler.sendMessage(msg);
+			GimbalAdapter.shared().start();
 		}
 		
-		return START_REDELIVER_INTENT;
+		return START_STICKY;
 	}
 	
 	@Override
 	public void onDestroy(){
-		Message msg = serviceHandler.obtainMessage(MSG_INTENT_JOB_FINISHED);
-		serviceHandler.sendMessage(msg);
-		
-		serviceLooper.quit();
+		GimbalAdapter.shared().stop();
 		
 		super.onDestroy();
 	}
